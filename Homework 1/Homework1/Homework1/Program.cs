eeaafff91c290875aa5353ec8b29e1a130ba76c8
BFipe <ilya.maximov11@gmail.com>
2022-07-14 11:54:42 +0300

    Homework_1.1

---
+++ Homework 1/Homework1/Homework1/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Homework1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine(x*-1);
+            Console.ReadKey();
+        }
+    }
+}
