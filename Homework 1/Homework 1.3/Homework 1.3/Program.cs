2f95c0ebd32099cb1c526a5da55481165116f527
BFipe <ilya.maximov11@gmail.com>
2022-07-14 12:17:24 +0300

    Homework_1.3

---
+++ Homework 1/Homework 1.3/Homework 1.3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Homework_1._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int rasst, shir, kolvo = 0;
+            while (kolvo <= 1)
+            {
+                Console.WriteLine("Введите кол-во столбов (не меньше 2)");
+                kolvo = int.Parse(Console.ReadLine());
+            }
+            Console.WriteLine("Введите расстояние между столбами (в м)");
+            rasst = int.Parse(Console.ReadLine()) * 100;
+            Console.WriteLine("Введите ширину столбов (в см)");
+            shir = int.Parse(Console.ReadLine());
+            Console.WriteLine($"Расстояние между столбами - {shir * (kolvo - 2) + rasst * (kolvo - 1)}");
+            Console.ReadKey();
+        }
+    }
+}
