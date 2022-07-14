ba2dc237719648e50156e2a0b16d8c5fdc70b101
BFipe <ilya.maximov11@gmail.com>
2022-07-14 12:04:32 +0300

    Homework_1.2

---
+++ Homework 1/Homework 1.2/Homework 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Homework_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter 1st number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter 2nd number");
+            int y = int.Parse(Console.ReadLine());
+            if (x>y)
+            {
+                int temp = x;
+                x = y;
+                y = temp;
+            }
+            while (x<=y)
+            {
+                Console.WriteLine(x);
+                x++;
+            }
+            Console.ReadKey();
+        }
+    }
+}
