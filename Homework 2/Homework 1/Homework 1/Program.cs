ee67a3e889d7b1392d348d8e0683df0b2a7587ce
BFipe <ilya.maximov11@gmail.com>
2022-07-14 12:24:05 +0300

    Homework 2.1

---
+++ Homework 2/Homework 1/Homework 1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework_1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int anwser = 1;
+            Console.WriteLine("Enter your number");
+            int x = int.Parse(Console.ReadLine());
+            for (int i = 2; i <= x; i++)
+            {
+                anwser = anwser * i;
+            }
+            Console.WriteLine($"Factorial of {x} is {anwser}");
+        }
+    }
+}
