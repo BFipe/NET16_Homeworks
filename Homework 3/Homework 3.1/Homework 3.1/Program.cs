5398214ae551c4cb75f88046bd8021e432f3c5fd
BFipe <ilya.maximov11@gmail.com>
2022-07-16 17:42:02 +0300

    Homework 3.1

---
+++ Homework 3/Homework 3.1/Homework 3.1/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Homework_3._1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter first number");
+            int x = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number");
+            int y = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter operation ( +  -  *  /  %)");
+            char op = char.Parse(Console.ReadLine());
+            switch (op)
+            {
+                case '+':
+                    Console.WriteLine(x + y);
+                    break;
+                case '-':
+                    Console.WriteLine(x - y);
+                    break;
+                case '*':
+                    Console.WriteLine(x * y);
+                    break;
+                case '/':
+                    Console.WriteLine(x / y);
+                    break;
+                case '%':
+                    Console.WriteLine(x % y);
+                    break;
+                default:
+                    Console.WriteLine("Wrong operation, try again");
+                    break;
+            }
+        }
+    }
+}
