f1e5dcc1c8a265a26f1b8a8df02f7cdd8f6a1d4e
BFipe <ilya.maximov11@gmail.com>
2022-07-21 10:25:25 +0300

    Homework task 4.2

---
+++ Homework 4/Homework 4.2/Homeework 4.2/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homeework_4._2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Введите размерность массива");
+            int mass_l = int.Parse(Console.ReadLine());
+            int[] mass = new int[mass_l];
+            for (int i = 0; i < mass_l; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} эллемент");
+                mass[i] = int.Parse(Console.ReadLine());
+            }
+
+            Stack<int> stack = new Stack<int>(mass);
+
+            foreach (int number in stack)
+            {
+                Console.WriteLine(number);
+            }
+        }
+    }
+}
