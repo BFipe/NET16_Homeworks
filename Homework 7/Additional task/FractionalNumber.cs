11241a101b48c7f122969f57658063577e80d39b
BFipe <ilya.maximov11@gmail.com>
2022-08-01 16:35:57 +0300

    Additional task homework 7

---
+++ Homework 7/Additional task/FractionalNumber.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Additional_task
+{
+    internal class FractionalNumber
+    {
+        private int _fraction;
+        public FractionalNumber(int integer, int fraction)
+        {
+            Integer = integer;
+            Fraction = fraction;
+        }
+        public int Fraction
+        {
+            get { return _fraction; }
+            set
+            {
+                if (value <= 0)
+                {
+                    _fraction = 1;
+                }
+                else
+                {
+                    _fraction = value;
+                }
+            }
+        }
+
+        public int Integer { get; set; }
+
+        public FractionalNumber Simplify(out FractionalNumber FractionalNumberSimplified)
+        {
+            int integerSimplified = Integer;
+            int fractionSimplified = Fraction;
+            int leastNumber;
+
+            if (Fraction > Integer)
+            {
+                leastNumber = Integer;
+            }
+            else
+            {
+                leastNumber=Fraction;
+            }
+
+
+            for (int i = 2; i <= leastNumber; i++)
+            {
+                if (Fraction % i == 0 && Integer % i == 0)
+                {
+                    integerSimplified = Integer / i;
+                    fractionSimplified = Fraction / i;
+                }
+            }
+            FractionalNumberSimplified = new FractionalNumber(integerSimplified, fractionSimplified);
+            return FractionalNumberSimplified;
+        }
+
+        public FractionalNumber Add(FractionalNumber FractionalNumber2, out FractionalNumber Anwser)
+        {
+            Anwser = new FractionalNumber(Integer * FractionalNumber2.Fraction + FractionalNumber2.Integer * Fraction, Fraction * FractionalNumber2.Fraction);
+            Anwser.Simplify(out Anwser);
+            return Anwser; //Все равно не понял как работает это, если сюда спокойно можно подставить null
+        }
+
+        public FractionalNumber Substract(FractionalNumber FractionalNumber2, out FractionalNumber Anwser)
+        {
+            Anwser = new FractionalNumber(Integer * FractionalNumber2.Fraction - FractionalNumber2.Integer * Fraction, Fraction * FractionalNumber2.Fraction);
+            Anwser.Simplify(out Anwser);
+            return Anwser;
+        }
+
+        public FractionalNumber Multiply(FractionalNumber FractionalNumber2, out FractionalNumber Anwser)
+        {
+            Anwser = new FractionalNumber(Integer * FractionalNumber2.Integer, Fraction * FractionalNumber2.Fraction);
+            Anwser.Simplify(out Anwser);
+            return Anwser;
+        }
+
+        public FractionalNumber Divide(FractionalNumber FractionalNumber2, out FractionalNumber Anwser)
+        {
+            Anwser = new FractionalNumber(Integer * FractionalNumber2.Fraction, Fraction * FractionalNumber2.Integer);
+            Anwser.Simplify(out Anwser);
+            return Anwser;
+        }
+    }
+}
---
+++ Homework 7/Additional task/Main program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Additional_task
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            FractionalNumber test1 = new FractionalNumber(22, 44);
+            FractionalNumber test2 = new FractionalNumber(11, 56);
+            FractionalNumber anwser;
+
+            Console.WriteLine($"{test1.Integer}/{test1.Fraction}");
+            test1.Simplify(out test1); //Я не понял как изменять значения сразу через класс, поэтому юзаю out
+            Console.WriteLine($"{test1.Integer}/{test1.Fraction}");
+
+            Console.WriteLine("_________________________");
+
+            test1.Divide(test2, out anwser);
+            Console.WriteLine($"Divide - {anwser.Integer}/{anwser.Fraction}");
+
+            test1.Multiply(test2, out anwser); 
+            Console.WriteLine($"Multiply - {anwser.Integer}/{anwser.Fraction}");
+
+            test1.Add(test2, out anwser);
+            Console.WriteLine($"Add - {anwser.Integer}/{anwser.Fraction}");
+
+            test1.Substract(test2, out anwser);
+            Console.WriteLine($"Substract - {anwser.Integer}/{anwser.Fraction}");
+        }
+    }
+}
