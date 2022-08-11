756c8d6ed68d47a259f3a8d1f98e40753c0332c2
BFipe <ilya.maximov11@gmail.com>
2022-08-11 12:54:27 +0300

    Homework 9

---
+++ Homework 9/Homework 9/Employee.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_9
+{
+
+
+
+
+
+    public class Employee
+    {
+
+        private int _age;
+        private int _experience;
+
+        public Employee(string name, int age, int experience, bool isHaveHigherEducation)
+        {
+            Age = age;
+            Experience = experience;
+            Name = name;
+            IsHaveHigherEducation = isHaveHigherEducation;
+        }
+        public string Name { get; set; }
+        public bool IsHaveHigherEducation { get; set; }
+        public int Age
+        {
+            get
+            {
+                return _age;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _age = 0;
+                }
+                else
+                {
+                    _age = value;
+                }
+            }
+        }
+
+        public int Experience
+        {
+            get
+            {
+                return _experience;
+            }
+            set
+            {
+                if (value < 0)
+                {
+                    _experience = 0;
+                }
+                else
+                {
+                    _experience = value;
+                }
+            }
+        }
+
+
+        #region Реализация через метод
+        //public static void Salary(List<Employee> Employers)    
+        //{
+        //    int salary = 0;
+        //    foreach (var employer in Employers)
+        //    {
+        //        if (employer.IsHaveHigherEducation)
+        //        {
+
+        //            salary = (employer.Experience + 1) * 1250;
+
+        //        }
+        //        else
+        //        {
+        //            salary = (employer.Experience + 1) * 1000;
+        //        }
+
+        //        Console.WriteLine($"{employer.Name} have salary {salary}");
+        //    }
+        //}
+        #endregion
+
+        public static int Salary(Employee employer)
+        {
+            if (employer.IsHaveHigherEducation)
+            {
+                return (employer.Experience + 1) * 1250;
+            }
+            else
+            {
+                return (employer.Experience + 1) * 1000;
+            }
+        }
+
+        public static void ShowSalary(List<Employee> Employers, EmployerDelegate salaryDelegate)
+        {
+            int salary = 0;
+            foreach (var employer in Employers)
+            {
+                salary = salaryDelegate(employer);
+                Console.WriteLine($"{employer.Name} have salary {salary}");
+            }
+        }
+
+
+
+    }
+}
---
+++ Homework 9/Homework 9/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Homework_9
+{
+    public delegate int EmployerDelegate(Employee employer);
+
+    internal class Program
+    {
+
+
+
+        static void Main(string[] args)
+        {
+            EmployerDelegate salaryDelegate = new EmployerDelegate(Employee.Salary);
+            Employee emp = new Employee("d", 2, 2, true);
+            List<Employee> employers = new List<Employee>
+            {
+                new Employee("Ilya", 19, 1, false),
+                new Employee("Egor", 29, 10, true),
+                new Employee("Andrey", 22, 5, true),
+                new Employee("Alisa", 24, 2, false),
+                new Employee("Nastya", 19, 0, false),
+                new Employee("German", 23, 4, true),
+            };
+
+            Employee.ShowSalary(employers, salaryDelegate);
+
+
+            Console.WriteLine("---------------");
+/////////////////////////////////////////////////////////////////////////////////////
+            EmployerDelegate salaryAnotherCompany = SalaryForAnotherCompany;
+
+
+            static int SalaryForAnotherCompany(Employee employer)
+            {
+                if (employer.IsHaveHigherEducation)
+                {
+                    return (employer.Experience + 1) * 1000;
+                }
+                else
+                {
+                    return (employer.Experience + 1) * 800;
+                }
+            }
+
+
+            Employee.ShowSalary(employers, salaryAnotherCompany);
+/////////////////////////////////////////////////////////////////////////////////////
+            Console.WriteLine("---------------");
+/////////////////////////////////////////////////////////////////////////////////////
+            EmployerDelegate anotherSalaryAnotherCompany = (Employee employer) =>
+            {
+                if (employer.IsHaveHigherEducation)
+                {
+                    return (employer.Experience + 1) * 12135;
+                }
+                else
+                {
+                    return (employer.Experience + 1) * 833;
+                }
+            };
+
+            Employee.ShowSalary(employers, anotherSalaryAnotherCompany);
+/////////////////////////////////////////////////////////////////////////////////////
+        }
+    }
+}
