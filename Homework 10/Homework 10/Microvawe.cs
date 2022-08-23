390a04e9c537feeee396600ae045811f0bb96177
BFipe <ilya.maximov11@gmail.com>
2022-08-23 12:39:07 +0300

    Homework 10

---
+++ Homework 10/Homework 10/Microvawe.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Homework_10
+{
+    public delegate void NotifyWarmUpCompleted(string dishName, int minuites);
+    internal class Microwave
+    {
+        public event NotifyWarmUpCompleted WarmUpCompleted;
+
+
+        public void WarmUp(string dishName, int minuites)
+        {
+            if (minuites < 0)
+            {
+                return;
+            }
+            Console.WriteLine($"Microvawing {dishName} for {minuites} min. mmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmmm");
+
+            if (WarmUpCompleted != null)
+            {
+                WarmUpCompleted.Invoke(dishName, minuites);
+            }
+        }
+    }
+}
---
+++ Homework 10/Homework 10/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Homework_10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Microwave microwave = new Microwave();
+            microwave.WarmUpCompleted += WarmUpCompletedHandler;
+            microwave.WarmUp("chickmn ngts", 6);
+        }
+
+        private static void WarmUpCompletedHandler(string dishName, int minuites)
+        {
+            Console.WriteLine($"i microwaved ur {dishName} for {minuites} min. Its still cold but i microvawed ur plate");
+        }
+    }
+}
